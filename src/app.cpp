7184011455a62e26a177b1f39c1bd8b4ce5f5a3b
Slava Aseev <sockskilling@gmail.com>
2023-10-11 23:39:53 +0300

    Тест SFML

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    sf::Window wnd(sf::VideoMode(800, 600), "test window");
+    wnd.setActive();
+
+    bool running = true;
+    while (running) {
+        sf::Event event;
+        while (wnd.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                running = false;
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        wnd.display();
+    }
 }
