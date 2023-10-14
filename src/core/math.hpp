5603dfd1d436ee2d463c3f52f794e422324d4241
Slava Aseev <sockskilling@gmail.com>
2023-10-14 03:11:28 +0300

    core/math: Add some functions

--- src/core/math.hpp
+++ src/core/math.hpp
@@ -10,6 +10,10 @@ inline auto lerp(auto v0, auto v1, auto t) {
     return v0 * (1.f - t) + v1 * t;
 }
 
+inline auto inverse_lerp(auto x1, auto x2, auto value) {
+    return (value - x1) / (x2 - x1);
+}
+
 inline float magnitude2(const sf::Vector2f& vec) {
     return vec.x * vec.x + vec.y * vec.y;
 }
@@ -31,6 +35,11 @@ namespace dependence {
     template <typename T>
     T quadratic(T value) {
         return value * value;
+    }
+
+    template <typename T>
+    T square(T value) {
+        return std::pow(value, T(0.5));
     }
 } // namespace dependence
 } // namespace core
