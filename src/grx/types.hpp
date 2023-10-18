6fb183524c3ac0edb7b5c877f8c5c88de714c021
Slava Aseev <sockskilling@gmail.com>
2023-10-18 23:53:19 +0300

    grx: Add aliases for some commonly used types

---
+++ src/grx/types.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "core/vec.hpp"
+
+namespace grx {
+    using core::vec2f;
+    using core::vec2u;
+    using core::vec2i;
+    using core::vec3f;
+    using core::vec3u;
+    using core::vec3i;
+    using core::rgba_t;
+}
